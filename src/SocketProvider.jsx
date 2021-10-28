e9630c87ad3ae555f134da444546b37ce8cc285a
jakob <drusany.jakob@gmail.com>
2021-10-28 12:44:00 +0200

    Added server_link.js

--- src/SocketProvider.jsx
+++ src/SocketProvider.jsx
@@ -6,6 +6,7 @@ import CreateGame from "./screens/createGame";
 import { io } from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
 import { setState } from "./redux/local_player_stuff";
+import { link } from "./server_link";
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -14,7 +15,7 @@ function useQuery() {
 const SocketProvider = () => {
   const gameState = useSelector((state) => state.lps);
   const dispatch = useDispatch();
-  const api_link = useQuery().get("server") || `http://localhost:5000/`;
+  const api_link = useQuery().get("server") || link;
   useEffect(() => {
     if (gameState.api_location != api_link) {
       dispatch(setState({ api_location: api_link }));
